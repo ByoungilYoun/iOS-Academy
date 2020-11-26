88968b905bf43e77af405d1b896a2c9bb9720eb0
ByoungilYoun <jimmy7525@naver.com>
2020-11-27 01:21:33 +0900

    Setup SafariServices in Viewcontroller

--- WebViewPractice/WebViewPractice/ViewController.swift
+++ WebViewPractice/WebViewPractice/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  
+  let safariButton : UIButton = {
+    let bt = UIButton()
+    bt.setTitle("Go to Apple", for: .normal)
+    bt.setTitleColor(.black, for: .normal)
+    bt.backgroundColor = .yellow
+    bt.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    return bt
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.addSubview(safariButton)
+    
+    safariButton.translatesAutoresizingMaskIntoConstraints = false
+    safariButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    safariButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+  
+  @objc func buttonTapped() {
+    let vc = SFSafariViewController(url: URL(string: "https://www.apple.com")!)
+    present(vc, animated: true, completion: nil)
   }
 
 
 }
 
+ 
