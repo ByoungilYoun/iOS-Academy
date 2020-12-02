0f854ef004dac417b3f518b719ddacb621b5c3b6
YounByoungil <jimmy7525@naver.com>
2020-12-03 01:50:56 +0900

    Setup StretchyTableHeaderView

--- StretchyTableViewHeader/StretchyTableViewHeader/ViewController.swift
+++ StretchyTableViewHeader/StretchyTableViewHeader/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  private let tableView : UITableView = {
+    let tableView = UITableView()
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    return tableView
+  }()
+  
+  let models = [
+    "New York",
+    "London",
+    "Hong Kong",
+    "Seoul",
+    "LA",
+    "New York",
+    "London",
+    "Hong Kong",
+    "Seoul",
+    "LA"
+  ]
+  //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.addSubview(tableView)
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.frame = view.bounds
+    
   }
+}
 
+  //MARK: - UITableViewDelegate
+extension ViewController : UITableViewDelegate {
+  
+}
 
+  //MARK: - UITableViewDataSource
+extension ViewController : UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return models.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = models[indexPath.row]
+    return cell
+  }
+  
+  
 }
 
