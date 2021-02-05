b63d282a9fde2e0c332e5446e94ff7481269a2d8
YounByoungil <jimmy7525@naver.com>
2021-02-05 10:52:52 +0900

    feat : Basic requestReview

--- AppRatingPrompt/AppRatingPrompt/ViewController.swift
+++ AppRatingPrompt/AppRatingPrompt/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
+  
+  //MARK: - Properties
+  private let button : UIButton = {
+    let button = UIButton()
+    button.setTitle("Rate Now", for: .normal)
+    button.backgroundColor = .systemBlue
+    button.setTitleColor(.white, for: .normal)
+    return button
+  }()
 
+  //MARK: - viewDidLoad()
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.addSubview(button)
+    
+    button.frame = CGRect(x: 0, y: 0, width: 200, height: 55)
+    button.center = view.center
+    button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    
   }
 
+  //MARK: - @objc func didTapButton
+  @objc private func didTapButton() {
+    guard let scene = view.window?.windowScene else {
+      print("no scene")
+      return
+    }
+    
+    SKStoreReviewController.requestReview(in: scene)
+  }
 
 }
 
