82f6f3b0e517d48bacf69985527020c9f9106539
YounByoungil <jimmy7525@naver.com>
2021-06-06 00:58:51 +0900

    how to delete file

--- FileManagerTutorial/FileManagerTutorial/ViewController.swift
+++ FileManagerTutorial/FileManagerTutorial/ViewController.swift
@@ -20,10 +20,22 @@ class ViewController: UIViewController {
     let newFolderUrl = url.appendingPathComponent("ios-academy") // ios-academy 라는 폴더 url 생성
 //    let secondSubFolder = url.appendingPathComponent("second-test").appendingPathComponent("ios-academy").appendingPathComponent("ios-academy").appendingPathComponent("ios-academy") // 폴더 타고 타고 들어가는 구조
     
-    let fileUrl = newFolderUrl.appendingPathComponent("logs. txt") // logs.txt 라는 파일 생성
-    let data = "Writing text to a text file!".data(using : .utf8) // 텍스트 string 을 data 로 생성해서 content 에 넣으면 적힌다.
-    manager.createFile(atPath: fileUrl.path, contents: data, attributes: [FileAttributeKey.creationDate: Date()])
+    // logs.text 에 string text 넣기
+//    let fileUrl = newFolderUrl.appendingPathComponent("logs. txt") // logs.txt 라는 파일 생성
+//    let data = "Writing text to a text file!".data(using : .utf8) // 텍스트 string 을 data 로 생성해서 content 에 넣으면 적힌다.
+//    manager.createFile(atPath: fileUrl.path, contents: data, attributes: [FileAttributeKey.creationDate: Date()])
     
+    
+    // 2. delete
+    let fileUrl = url.appendingPathComponent("ios-academy")
+    if manager.fileExists(atPath: fileUrl.path) {
+      do {
+        try manager.removeItem(at: fileUrl)
+      }
+      catch {
+        print(error)
+      }
+    }
   }
 
 
