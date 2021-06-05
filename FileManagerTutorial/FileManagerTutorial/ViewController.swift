1c3ce233e18ad057c0d65abd0c727162720bdae2
YounByoungil <jimmy7525@naver.com>
2021-06-05 23:57:37 +0900

    Create Folders in Directory

--- FileManagerTutorial/FileManagerTutorial/ViewController.swift
+++ FileManagerTutorial/FileManagerTutorial/ViewController.swift
@@ -11,7 +11,24 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    
+    let manager = FileManager.default
+    guard let url = manager.urls(for: .documentDirectory, in: .userDomainMask).first else {return}
+    
+    
+    let newFolderUrl = url.appendingPathComponent("ios-academy") // ios-academy 라는 폴더 url 생성
+    let secondSubFolder = url.appendingPathComponent("second-test").appendingPathComponent("ios-academy").appendingPathComponent("ios-academy").appendingPathComponent("ios-academy") // 폴더 타고 타고 들어가는 구조
+    
+    do {
+      // 디렉토리 폴더 생성 
+      try manager.createDirectory(at: newFolderUrl, withIntermediateDirectories: true, attributes: [:])
+      try manager.createDirectory(at: secondSubFolder, withIntermediateDirectories: true, attributes: [:])
+    }
+    catch {
+      print(error)
+    }
+    
   }
 
 
