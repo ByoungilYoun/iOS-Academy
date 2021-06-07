2bdff611a60ccc2099fdc04721c8ea40a6337105
YounByoungil <jimmy7525@naver.com>
2021-06-07 23:17:32 +0900

    Setup Project

---
+++ ConfettiAnimationEffectTutorial/ConfettiAnimationEffectTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConfettiAnimationEffectTutorial
+//
+//  Created by 윤병일 on 2021/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
