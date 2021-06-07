041b44e11efb6a9da8ca774803344260c7f57acc
YounByoungil <jimmy7525@naver.com>
2021-06-07 23:53:27 +0900

    Create EmitterCell with many Colors

--- ConfettiAnimationEffectTutorial/ConfettiAnimationEffectTutorial/ViewController.swift
+++ ConfettiAnimationEffectTutorial/ConfettiAnimationEffectTutorial/ViewController.swift
@@ -14,26 +14,39 @@ class ViewController: UIViewController {
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    createLayer()
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+    view.addSubview(button)
+    button.backgroundColor = .systemBlue
+    button.center = view.center
+    button.setTitle("FINISH", for: .normal)
+    button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
   }
   
   //MARK: - Functions
   private func createLayer() {
     let layer = CAEmitterLayer()
-    layer.emitterPosition = CGPoint(x: view.center.x, y: view.center.y) // where to start
+    layer.emitterPosition = CGPoint(x: view.center.x, y: -100) // where to start
     
-    let cell = CAEmitterCell()
-    cell.scale = 0.1
-    cell.emissionRange = .pi * 2
-    cell.lifetime = 20 // lifetime of the cell
-    cell.birthRate = 100
-    cell.velocity = 200 // 속도
-    cell.color = UIColor.systemPink.cgColor // Important! 하얀 이미지를 사용해야 색이 render 되서 입혀진다.
-    cell.contents = UIImage(named: "image2")!.cgImage
+    let colors : [UIColor] = [.systemRed, .systemBlue, .systemOrange, .systemGreen, .systemPink, .systemYellow]
     
-    layer.emitterCells = [cell]
+    let cells : [CAEmitterCell] = colors.compactMap {
+      let cell = CAEmitterCell()
+      cell.scale = 0.05 // 크기
+      cell.emissionRange = .pi * 2
+      cell.lifetime = 10 // lifetime of the cell
+      cell.birthRate = 50
+      cell.velocity = 150 // 속도
+      cell.color = $0.cgColor // Important! 하얀 이미지를 사용해야 색이 render 되서 입혀진다.
+      cell.contents = UIImage(named: "image2")!.cgImage
+      return cell
+    }
+    
+    layer.emitterCells = cells
     view.layer.addSublayer(layer)
   }
 
+  @objc private func didTapButton() {
+    createLayer()
+  }
 }
 
